9f457285859ff3e20256b7697c9baee805d3f81f
Karan <karannagra9924@gmail.com>
2022-03-16 19:13:46 +0530

    Medicine

---
+++ app/src/main/java/com/rawtooth/medicineapp/cart/EnterPatientDetails.kt
@@ -0,0 +1,11 @@
+package com.rawtooth.medicineapp.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EnterPatientDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enter_patient_details)
+    }
+}
---
+++ app/src/main/java/com/rawtooth/medicineapp/cart/PatientDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.rawtooth.medicineapp.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rawtooth.medicineapp.R
+
+class PatientDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_details)
+    }
+}

132d97f5ee6c595d42ab41afe87dff13518cd612
Karan <karannagra9924@gmail.com>
2022-03-21 20:19:08 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/rawtooth/medicineapp/activity_login.kt
@@ -0,0 +1,11 @@
+package com.rawtooth.medicineapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

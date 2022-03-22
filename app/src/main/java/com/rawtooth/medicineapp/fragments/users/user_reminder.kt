079cacf8279e4fe96b94f48603f70054efa22303
Karan <karannagra9924@gmail.com>
2022-03-22 21:16:18 +0530

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/rawtooth/medicineapp/fragments/users/user_reminder.kt
+++ app/src/main/java/com/rawtooth/medicineapp/fragments/users/user_reminder.kt
@@ -1,12 +1,141 @@
 package com.rawtooth.medicineapp.fragments.users
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.rawtooth.medicineapp.R
+import android.widget.Toast
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+
+import com.rawtooth.medicineapp.databinding.ActivityUserReminderBinding
+import java.util.*
 
 class user_reminder : AppCompatActivity() {
+    lateinit var binding:ActivityUserReminderBinding
+    private lateinit var picker : MaterialTimePicker
+    private lateinit var calendar: Calendar
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_reminder)
+        binding= ActivityUserReminderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createNotificationChannel()
+
+        binding.selectTimeBtn.setOnClickListener {
+
+            showTimePicker()
+
+        }
+        binding.setAlarmBtn.setOnClickListener {
+
+            setAlarm()
+
+        }
+
+        binding.cancelAlarmBtn.setOnClickListener {
+
+            cancelAlarm()
+
+        }
+
+    }
+
+    private fun cancelAlarm() {
+
+
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this,AlarmReceiver::class.java)
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent,0)
+
+        alarmManager.cancel(pendingIntent)
+        Toast.makeText(this,"Alarm Cancelled",Toast.LENGTH_LONG).show()
+
+
+
+    }
+
+    private fun setAlarm() {
+
+
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this,AlarmReceiver::class.java)
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent,0)
+
+        alarmManager.setRepeating(
+
+            AlarmManager.RTC_WAKEUP,calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,pendingIntent
+
+        )
+
+        Toast.makeText(this,"Alarm set Successfuly",Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun showTimePicker() {
+
+        picker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Select Alarm Time")
+            .build()
+
+        picker.show(supportFragmentManager,"medicine")
+
+        picker.addOnPositiveButtonClickListener {
+
+            if (picker.hour > 12){
+
+                binding.selectedTime.text =
+                    String.format("%02d",picker.hour - 12) + " : " + String.format(
+                        "%02d",
+                        picker.minute
+                    ) + "PM"
+
+
+            }else{
+
+                String.format("%02d",picker.hour) + " : " + String.format(
+                    "%02d",
+                    picker.minute
+                ) + "AM"
+
+            }
+
+            calendar = Calendar.getInstance()
+            calendar[Calendar.HOUR_OF_DAY] = picker.hour
+            calendar[Calendar.MINUTE] = picker.minute
+            calendar[Calendar.SECOND] = 0
+            calendar[Calendar.MILLISECOND] = 0
+
+        }
+
+    }
+
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val name : CharSequence = "foxandroidReminderChannel"
+            val description = "Channel For Alarm Manager"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("foxandroid",name,importance)
+            channel.description = description
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+
+            notificationManager.createNotificationChannel(channel)
+        }
+
     }
 }

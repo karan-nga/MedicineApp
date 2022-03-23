de92a4763d7553339e51fd9a3dc15a26704a77af
Karan <karannagra9924@gmail.com>
2022-03-23 21:10:22 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ app/src/main/java/com/rawtooth/anchal/insurance/Messages.kt
@@ -0,0 +1,12 @@
+package com.rawtooth.anchal.insurance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rawtooth.medicineapp.R
+
+class Messages : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_messages)
+    }
+}

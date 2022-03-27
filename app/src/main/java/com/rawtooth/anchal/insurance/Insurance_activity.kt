b0c1002a8fbb9c1c6324a4d46983258807161bff
Karan <karannagra9924@gmail.com>
2022-03-27 22:38:09 +0530

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/rawtooth/anchal/insurance/Insurance_activity.kt
+++ app/src/main/java/com/rawtooth/anchal/insurance/Insurance_activity.kt
@@ -6,17 +6,16 @@ import android.os.Bundle
 import com.rawtooth.medicineapp.R
 import com.rawtooth.medicineapp.databinding.ActivityInsuranceBinding
 
-class
-Insurance_activity : AppCompatActivity() {
+class Insurance_activity : AppCompatActivity() {
     lateinit var binding:ActivityInsuranceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityInsuranceBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.rec60.setOnClickListener{
+        binding.buyNewPlan.setOnClickListener{
             startActivity(Intent(this,Insurance_Details::class.java))
         }
-        binding.rec59.setOnClickListener{
+        binding.buyhealthcard.setOnClickListener{
             startActivity(Intent(this,BuildYourHealthPlan::class.java))
         }
     }
--- app/src/main/java/com/rawtooth/medicineapp/fragments/Insurancee.kt
+++ app/src/main/java/com/rawtooth/medicineapp/fragments/Insurancee.kt
@@ -1,10 +1,13 @@
 package com.rawtooth.medicineapp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.rawtooth.anchal.insurance.BuildYourHealthPlan
+import com.rawtooth.anchal.insurance.Insurance_Details
 import com.rawtooth.medicineapp.R
 import com.rawtooth.medicineapp.databinding.FragmentInsuranceeBinding
 
@@ -16,7 +19,12 @@ lateinit var binding:FragmentInsuranceeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding= FragmentInsuranceeBinding.inflate(layoutInflater)
-
+        binding.buyNewPlan.setOnClickListener{
+            startActivity(Intent(context, Insurance_Details::class.java))
+        }
+        binding.buyhealthcard.setOnClickListener{
+            startActivity(Intent(context, BuildYourHealthPlan::class.java))
+        }
 
 
         return binding.root }

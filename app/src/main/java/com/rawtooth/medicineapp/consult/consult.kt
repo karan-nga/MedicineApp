910481b0bd0fce9fb1fe4683c228a06170de9924
Karan <karannagra9924@gmail.com>
2022-03-29 18:32:34 +0530

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/rawtooth/medicineapp/consult/consult.kt
+++ app/src/main/java/com/rawtooth/medicineapp/consult/consult.kt
@@ -1,12 +1,18 @@
 package com.rawtooth.medicineapp.consult
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.rawtooth.medicineapp.R
 
 class consult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consult)
+    }
+
+    fun bookDoctor(view: View) {
+        startActivity(Intent(this,doctor_consult::class.java))
     }
 }
